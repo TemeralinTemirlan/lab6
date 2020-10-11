4575c3c7e1f32ff769c723ca3ec44494beee7da8
Temeralin Temirlan <jestklq@gmail.com>
2020-10-11 20:50:44 +0600

    factorial function

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ function add(a, b) {
     return a + b
 }
 
-<<<<<<< HEAD
-function multiply(a, b) {
-    return a * b
-=======
 function subtract(a, b) {
     return a - b
->>>>>>> 0d62b36c4052c1342c4303144f57adfb4420898e
 }
 
 function divide(a, b) {
     return a / b
+}
+
+
+function factorial(a) {
+    if (a == 1) {
+        return 1
+    } else {
+        return a * factorial(a - 1)
+    }
 }
